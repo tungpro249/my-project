7c425936b310f674b1b8a2b0885b540948633e25
Doan Thanh Tung <tung.doan@minhtrithanh.vn>
2025-05-02 14:31:49 +0700

    delete i18n

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-import { i18n } from './next-i18next.config';
 
 const nextConfig = {
-  i18n,
   experimental: {
     appDir: true, // vì bạn đang dùng src/app
-  },
-  transpilePackages: ['next-i18next'],
+  }
 };
 
 export default nextConfig;
