3b7571c314ad5ab10d55a974a98bbd842c0a75b0
thanh tung <tungt3922gmail.com>
2025-05-14 06:53:18 +0700

    push

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -2,7 +2,7 @@ import { Layout, Row, Col, Typography, Card, Button } from "antd";
 import Link from "next/link";
 
 export default async function BlogPage() {
-  const res = await fetch("https://backend-nest-og78.onrender.com/post", { cache: "no-store" });
+  const res = await fetch("http://localhost:5000/post", { cache: "no-store" });
   const blogPosts = await res.json();
 
   return (
@@ -17,8 +17,8 @@ export default async function BlogPage() {
               title={<span className="!text-xl !font-semibold">{post.title}</span>}
               className="shadow-md rounded-lg hover:shadow-lg transition-shadow duration-300"
             >
-              <p className="block mb-4">{post.description}</p>
-              <Link href={`/blog/${post.slug}`}>
+              <p className="block mb-4">{post.content}</p>
+              <Link href={`/post/${post.id}`}>
                 <Button type="primary" className="w-full">
                   Đọc tiếp
                 </Button>
