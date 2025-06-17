8bd7b9688f7bfa6457b1ff4e5ac53723e3188f53
Doan Thanh Tung <tung.doan@minhtrithanh.vn>
2025-06-17 17:28:21 +0700

    push

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Layout } from "antd";
-import { HomePage } from "./home/page";
+import HomePage  from "./home/page";
 
 export default function App() {
   return (
